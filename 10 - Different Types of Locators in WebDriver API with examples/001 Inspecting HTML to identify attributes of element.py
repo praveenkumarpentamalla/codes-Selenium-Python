81fc44129c99118678ff7f75fb4c6bd5cd1c7ff6
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-07 17:06:30 +0530

    Update 001 Inspecting HTML to identify attributes of element.py

--- 10 - Different Types of Locators in WebDriver API with examples/001 Inspecting HTML to identify attributes of element.py
+++ 10 - Different Types of Locators in WebDriver API with examples/001 Inspecting HTML to identify attributes of element.py
@@ -1,21 +1,59 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By 
 
-# Automatically downloads compatible driver
+# Automatically download and setup the ChromeDriver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-driver.get("https://www.rahulshettyacademy.com/practice")  
+# Open the website
+driver.get("https://management.divsolution.com/")
+driver.maximize_window()
 
-# Fill the form  
-driver.find_element_by_name("name").send_keys("Rahul")  
-driver.find_element_by_name("email").send_keys("test@example.com")  
-driver.find_element_by_id("password").send_keys("123456")  
+# Print title and URL
+print("Title:", driver.title)
+print("URL:", driver.current_url)
 
-# Select checkbox  
-driver.find_element_by_id("agreeTerms").click()  
+# Locate the email input using the correct name or ID
+# From your HTML: name="email_temp", id="id_email"
+driver.find_element(By.NAME, "email_temp").send_keys("praveenkumar")
 
-# Submit form  
-driver.find_element_by_css_selector("#submitBtn").click()  
+# Alternatively, using ID:
+# driver.find_element(By.ID, "id_email").send_keys("praveenkumar")
 
-driver.quit()  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+
+# # Automatically downloads compatible driver
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+# driver.get("https://www.rahulshettyacademy.com/practice")  
+
+# # Fill the form  
+# driver.find_element_by_name("name").send_keys("Rahul")  
+# driver.find_element_by_name("email").send_keys("test@example.com")  
+# driver.find_element_by_id("password").send_keys("123456")  
+
+# # Select checkbox  
+# driver.find_element_by_id("agreeTerms").click()  
+
+# # Submit form  
+# driver.find_element_by_css_selector("#submitBtn").click()  
+
+# driver.quit()  
