8b681e810f025f82c4cd9a0a263e9cd2a49bfa54
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-08 14:36:20 +0530

    Create 002 Introduction to CSS Selector and name locators with example.py

---
+++ 10 - Different Types of Locators in WebDriver API with examples/002 Introduction to CSS Selector and name locators with example.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+# Automatically downloads compatible driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://sso.teachable.com/secure/9521/identity/sign_up/otp")
+driver.maximize_window()
+
+print("Title:", driver.title)
+print("URL:", driver.current_url)
+
+
+driver.find_element(By.CSS_SELECTOR, "#name").send_keys("praveenkumar")
+driver.find_element(By.CSS_SELECTOR, "#allowMarketingEmails").click()
+
+driver.maximize_window()
+driver.minimize_window()
