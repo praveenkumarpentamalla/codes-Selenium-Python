e59fd4737bdb2785311b52bf9408bedd92a6d17e
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-08 15:47:48 +0530

    Create 005 Identifying Links with Text and building Smart CSS around it.py

---
+++ 10 - Different Types of Locators in WebDriver API with examples/005 Identifying Links with Text and building Smart CSS around it.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+
+# Automatically downloads compatible driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://login.salesforce.com/?locale=in")
+
+driver.find_element(By.ID, "username").send_keys("PraveenKaumar")
+driver.find_element(By.CSS_SELECTOR, "#password").send_keys("kumar")
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR, "#password").clear()
+driver.find_element(By.LINK_TEXT, "Forgot Your Password?").click()
+
+
+print("Title:", driver.title)
+print("URL:", driver.current_url)
+
+time.sleep(2)
+
