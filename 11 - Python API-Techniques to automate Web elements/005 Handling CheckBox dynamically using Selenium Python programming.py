ab4ae374260740758eba7f66072e112fc5266c2c
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-11 12:51:11 +0530

    Create 005 Handling CheckBox dynamically using Selenium Python programming.py

---
+++ 11 - Python API-Techniques to automate Web elements/005 Handling CheckBox dynamically using Selenium Python programming.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+checkboxes = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
+
+print(len(checkboxes))
+
+
+for checkbox in checkboxes:
+    checkbox.click()
+    assert checkbox.is_selected()
+
