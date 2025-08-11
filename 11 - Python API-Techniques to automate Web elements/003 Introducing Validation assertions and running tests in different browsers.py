a45af67bfa250f5c9a6fb281ac48971744b82bb7
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-11 11:03:04 +0530

    Create 003 Introducing Validation assertions and running tests in different browsers.py

---
+++ 11 - Python API-Techniques to automate Web elements/003 Introducing Validation assertions and running tests in different browsers.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+# Automatically downloads compatible driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+
+driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("praveen")
+driver.find_element(By.NAME, "email").send_keys("praveen@example.com")
+driver.find_element(By.CSS_SELECTOR, "#exampleInputPassword1").send_keys("kumar")
+driver.find_element(By.ID, "exampleCheck1").click()
+
+time.sleep(2)
+
+dropdown = Select(driver.find_element(By.ID, "exampleFormControlSelect1"))
+dropdown.select_by_visible_text("Female")
+dropdown.select_by_index(0)
+# dropdown.select_by_value("M")
+
+time.sleep(2)
+
+driver.find_element(By.XPATH, "//input[@value='Submit']").click()
+
+# print(driver.find_element(By.CLASS_NAME, "alert-success").text )
+
+message = driver.find_element(By.CLASS_NAME, "alert-success").text
+
+assert "Success!" in message
+
+
+# print(driver.find_element(By.CSS_SELECTOR, "form[name='login'] label[for='password']").text)
+
+
+print("Title:", driver.title)
+print("URL:", driver.current_url)
+
+time.sleep(2)
+
