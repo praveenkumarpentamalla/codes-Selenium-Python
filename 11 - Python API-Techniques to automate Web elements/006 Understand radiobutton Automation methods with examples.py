d4317d939ec0ecb32fe3076e58ed19671cae7fd8
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-11 16:55:41 +0530

    Update 006 Understand radiobutton Automation methods with examples.py

--- 11 - Python API-Techniques to automate Web elements/006 Understand radiobutton Automation methods with examples.py
+++ 11 - Python API-Techniques to automate Web elements/006 Understand radiobutton Automation methods with examples.py
@@ -1,1 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
 
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+checkboxes = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
+
+print(len(checkboxes))
+
+
+for checkbox in checkboxes:
+    if checkbox.get_attribute("value") == "option2":
+        checkbox.click()
+        assert checkbox.is_selected()
+
+
+radiobutton = checkboxes = driver.find_elements(By.NAME, "radioButton")
+radiobutton[1].click()
+time.sleep(2)
+assert radiobutton[1].is_selected()
