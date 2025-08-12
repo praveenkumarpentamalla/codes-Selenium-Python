65e8406d031959c5b2f32bf8d8b68f7f73b1a8dd
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-12 12:43:19 +0530

    Create 003 What is Impilicit Wait in Selenium And its advantages.py

---
+++ 12 - Synchronization (Explicit & Implicit Waits) in Selenium webdriver/003 What is Impilicit Wait in Selenium And its advantages.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.implicitly_wait(5)
+
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+
+
+driver.find_element(By.CSS_SELECTOR, "input.search-keyword").send_keys("ber")
+
+time.sleep(4)
+
+count = len(driver.find_elements(By.XPATH, "//div[@class='products']/div"))
+assert count == 3
+
+buttons = driver.find_elements(By.XPATH, "//div[@class='product-action']/button")
+
+for button in buttons:
+    button.click()
+
+
+
+driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
+
+driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
+
+
+driver.find_element(By.CLASS_NAME, "promoCode").send_keys("rahulshettyacademy")
+
+driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+driver.find_element(By.CSS_SELECTOR, "span.promoInfo").text
