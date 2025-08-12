03987640ccbca446cc4f0ca26575578889a5d63f
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-12 14:22:52 +0530

    Create 003 Functional Automation example on GreenKart application - 2.py

---
+++ 13 - Deep Dive into Functional Automation using Python/003 Functional Automation example on GreenKart application - 2.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions 
+import time
+
+list = []
+list2 = []
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+
+
+driver.find_element(By.CSS_SELECTOR, "input.search-keyword").send_keys("ber")
+
+time.sleep(4)
+
+count = len(driver.find_elements(By.XPATH, "//div[@class='products']/div"))
+assert count == 3
+
+buttons = driver.find_elements(By.XPATH, "//div[@class='product-action']/button")
+
+
+for button in buttons:
+    list.append(button.find_element(By.XPATH, "parent::div/parent::div/h4").text)
+    button.click()
+
+print(list)
+
+
+driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
+
+driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
+
+wait = WebDriverWait(driver, 5)
+wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "promoCode")))
+
+
+veggies = driver.find_elements(By.CSS_SELECTOR, "p.product-name")
+
+for veg in veggies:
+    list2.append(veg.text)
+
+print(list2)
+
+
+assert list == list2
+
+driver.find_element(By.CLASS_NAME, "promoCode").send_keys("rahulshettyacademy")
+
+driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "span.promoInfo")))
+driver.find_element(By.CSS_SELECTOR, "span.promoInfo").text
