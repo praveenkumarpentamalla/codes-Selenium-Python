222e74d121a7dc033ad031fc10e9f08aab8468ec
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-12 14:12:28 +0530

    Update and rename 001 When to use Explicit Wait and IMplicit wait in your Tests.py to 002 Functional Automation example on GreenKart application - 1.py

---
+++ 13 - Deep Dive into Functional Automation using Python/002 Functional Automation example on GreenKart application - 1.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions 
+import time
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+
+
+driver.find_element(By.CSS_SELECTOR, "input.search-keyword").send_keys("ber")
+
+time.sleep(4)
+
+count = len(driver.find_elements(By.XPATH, "//div[@class='products']/div"))
+assert count == 3
+
+buttons = driver.find_elements(By.XPATH, "//div[@class='product-action']/button")
+
+
+for button in buttons:
+    print(button.find_element(By.XPATH, "parent::div/parent::div/h4").text)
+    button.click()
+
+
+
+driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
+
+driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
+
+wait = WebDriverWait(driver, 5)
+wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "promoCode")))
+
+driver.find_element(By.CLASS_NAME, "promoCode").send_keys("rahulshettyacademy")
+
+driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "span.promoInfo")))
+driver.find_element(By.CSS_SELECTOR, "span.promoInfo").text
+
