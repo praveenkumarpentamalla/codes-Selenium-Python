2db56282430c59f63184d695d38e1cb8539054fb
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-14 17:13:03 +0530

    Create 004 Taking Screenshots using Selenium Python with assertions.py

---
+++ 16 - End to End Practise Project with Complete methods in Selenium/004 Taking Screenshots using Selenium Python with assertions.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+import os
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+
+driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
+
+products = driver.find_elements(By.XPATH, "//div[@class='card h-100']")
+
+for product in products:
+    productName = product.find_element(By.XPATH, "div/h4/a").text
+    if productName == "Blackberry":
+        product.find_element(By.XPATH, "div/button").click()
+        
+
+driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
+driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
+driver.find_element(By.ID, "country").send_keys("ind")
+wait = WebDriverWait(driver, 7)
+wait.until(EC.presence_of_element_located((By.LINK_TEXT, "India")))
+driver.find_element(By.PARTIAL_LINK_TEXT, "India").click()
+driver.find_element(By.XPATH, "//div[@class='checkbox checkbox-primary']").click()
+driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+successText = driver.find_element(By.CLASS_NAME, "alert-success").text
+
+
+assert "Success! Thank you!" in successText
+
+driver.get_screenshot_as_file("screen.png")
