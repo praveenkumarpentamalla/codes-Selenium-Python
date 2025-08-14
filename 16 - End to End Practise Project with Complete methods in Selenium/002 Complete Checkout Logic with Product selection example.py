9d84fb153eb1ccb1e04c79a49f5a210297b13005
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-14 16:17:24 +0530

    Create 002 Complete Checkout Logic with Product selection example.py

---
+++ 16 - End to End Practise Project with Complete methods in Selenium/002 Complete Checkout Logic with Product selection example.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+
+driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
+
+products = driver.find_elements(By.XPATH, "//div[@class='card h-100']")
+
+for product in products:
+    productName = product.find_element(By.XPATH, "div/h4/a").text
+    if productName == "Blackberry":
+        product.find_element(By.XPATH, "div/button").click()
+        time.sleep(2)
+
+driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
+driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
+driver.find_element(By.ID, "country").send_keys("ind")
+time.sleep(2)
