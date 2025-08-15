6036c0b15abbbbf889c8cc68248963dff4fd144b
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-15 11:03:10 +0530

    Create test_demo3.py

---
+++ 18 - Part 1 Pytest - Unit Testing Framework of Python/test_demo3.py
@@ -0,0 +1,8 @@
+# Any pytest files shoud start with 'test_' or end with 'test_'
+# pytest method names should start with test
+# Any code should be wrapped in method only
+
+
+def test_firsttest():
+    msg = "Hello"
+    assert msg == "Hi"
