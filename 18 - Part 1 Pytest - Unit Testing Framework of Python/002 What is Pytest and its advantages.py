86820cadb2a57d3f20a11b8ee0b465f0aaa66a3e
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-15 09:37:20 +0530

    Update 002 What is Pytest and its advantages.py

--- 18 - Part 1 Pytest - Unit Testing Framework of Python/002 What is Pytest and its advantages.py
+++ 18 - Part 1 Pytest - Unit Testing Framework of Python/002 What is Pytest and its advantages.py
@@ -1,1 +1,8 @@
+# Any pytest files shoud start with 'test_' or end with 'test_'
+# pytest method names should start with test
+# Any code should be wrapped in method only
+
+
+def test_firsttest():
+    print("Hello")
 
