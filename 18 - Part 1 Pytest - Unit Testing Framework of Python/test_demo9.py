4d7d15130554af92f436d2c1d4fc2976b80dd80f
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-15 15:11:37 +0530

    Create test_demo9.py

---
+++ 18 - Part 1 Pytest - Unit Testing Framework of Python/test_demo9.py
@@ -0,0 +1,29 @@
+# Any pytest files shoud start with 'test_' or end with 'test_'
+# pytest method names should start with test
+# Any code should be wrapped in method only
+# Method name should have sense
+# -k stands for method names executions, -s logs in out put -v stands for more info metadata
+# you can run specific file with py.test <filename>
+# you can mark (tag) tests @pytest.mark.smoke and then run with -m 
+# you can skip with @pytest.mark.skip
+# @pytest.mark.xfail it do not show in output
+# fixtures are used as setup and tear dwon methods for test cases - conftest file to generalize
+# fixture and make it available to all test cases (fixture name into parameters of method)
+# datadriven and parameterization can be done with return statement in tuple format
+# when you define fixture scope to class it will run once before class is initiated and at the end
+
+
+
+import pytest
+
+@pytest.mark.usefixtures("dataLoad")
+class TestExample2:
+
+    def test_edit_profile(self, dataLoad):
+        print(dataLoad)
+        print(dataLoad[0])
+        print(dataLoad[2])
+
+
+def test_crossBrowser(crosBrowser):
+    print(crosBrowser)
