1459f07df97dd5ba8935dbd37525e7f3cccf089e
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-15 16:50:20 +0530

    Create BaseClass.py

---
+++ 19 - Part 2 - Logging and generating HTML Reports in Python Framework/BaseClass.py
@@ -0,0 +1,20 @@
+import logging
+
+
+class BaseClass:
+    def test_logging(self):
+
+        logger = logging.getLogger(__name__)
+        fileHandler = logging.FileHandler('logfile.log')
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s : %(message)s")
+        fileHandler.setFormatter(formatter)
+
+        logger.addHandler(fileHandler)
+
+        logger.setLevel(logging.DEBUG)
+        logger.debug("A debug statement is executed")
+        logger.info("Inforamation statement")
+        logger.warning("Something is in warning mode")
+        logger.error("A Major error has happened")
+        logger.critical("Critical issue")
+        return logger
