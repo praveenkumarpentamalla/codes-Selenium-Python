2b41922492a5dcce9772db09fb834bdc554f21a8
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-28 10:57:59 +0530

    Create HomePageData.py

---
+++ 20 - Part 3 - Develop End to End Selenium Python Framework from scratch/example-sample/HomePageData.py
@@ -0,0 +1,4 @@
+class HomePageData:
+
+    test_HomePage_data = [{"firstname": "Praveen", "lastname": "Kumar", "gender": "Male"}, {"firstname": "Kumar", "lastname": "Praveen", "gender": "Male"}]
+    
